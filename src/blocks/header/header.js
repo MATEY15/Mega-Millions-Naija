5fac55cb32c29e870d0fecda7fbba73ad2fd924e
Sergey <act1987@mail.ru>
2021-06-17 03:59:59 +0300

    Cart & Successful Game

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -28,3 +28,22 @@ headerHeight();
 $(window).on('resize', function(){
     headerHeight();
 });
+
+function dropCart() {
+    let cartItem = $('.header__cart');
+    let cartDrop = $('.header__cart-preview');
+    let cartDropActive = 'header__cart-preview--active';
+
+    cartItem.click(function(e){
+        e.preventDefault();
+        cartDrop.toggleClass(cartDropActive);
+    });
+    $(document).mouseup(function (e) {
+        let container = $(".header__cart-wrapper");
+        if (container.has(e.target).length === 0){
+            cartDrop.removeClass(cartDropActive);
+        }
+    });
+
+}
+dropCart()
